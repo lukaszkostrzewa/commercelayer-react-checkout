4a12c01a17efc281bdaed0d90a8ff3e41f67e667
Lukasz Kostrzewa <lukasz.webmaster@gmail.com>
2022-05-20 19:57:46 +0200

    Use i18next language detector

--- components/data/i18n/index.ts
+++ components/data/i18n/index.ts
@@ -1,9 +1,11 @@
 import i18n, { use } from "i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
 import translationEN from "public/static/locales/en/common.json"
 import translationIT from "public/static/locales/it/common.json"
+import translationPL from "public/static/locales/pl/common.json"
 import { initReactI18next } from "react-i18next"
 
-const languages = ["en", "it"]
+const languages = ["en", "it", "pl"]
 
 const resources = {
   en: {
@@ -12,11 +14,13 @@ const resources = {
   it: {
     translation: translationIT,
   },
+  pl: {
+    translation: translationPL,
+  },
 }
 
-use(initReactI18next).init({
+use(initReactI18next).use(LanguageDetector).init({
   resources,
-  lng: languages[0],
   fallbackLng: languages,
 })
 
